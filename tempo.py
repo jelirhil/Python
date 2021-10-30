6fd48b58097b9932caceec96d9c829bbce20e432
jelirhil <70600192+jelirhil@users.noreply.github.com>
2021-10-30 16:41:04 -0300

    Update tempo.py

--- tempo.py
+++ tempo.py
@@ -4,16 +4,19 @@ import requests
 import json
 from googletrans import Translator
 
+#ativa tradutor
 translator= Translator()
-#acquirer = TokenAcquirer()
+#escolha a cidade
 cidade = input('Escreva sua cidade:')
-
+#requer da API
 req = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+ cidade+ '&appid=cc77e3425c57f3802b4f95c9ee8071b4')
-
+#altera para json
 tempo = req.json()
-tempo_1 = (tempo['weather'][0]['main'])
-
-tempo_pt = translator.translate(tempo_1,dest='pt')
-print (tempo_pt.text)
+#recebe a temperatura e transforma em Celsius, era kelvin
 temp = (tempo['main']['temp']-273.15)
+tempo_1 = (tempo['weather'][0]['main'])
+#traduz
+tempo_pt = translator.translate(tempo_1,dest='pt')
+
+print (tempo_pt.text)
 print (f'A temperatura é de {temp:.2f}°')
